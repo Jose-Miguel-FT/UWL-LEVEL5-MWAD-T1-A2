a54007671d657bab21c238098f472ddc5e210dda
Jose-Miguel-FT <miguelfinol_48@hotmail.com>
2021-12-08 18:46:58 +0000

    3 files modified

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import React from 'react';
 import './style.css';
 import MedicalTreatment from './components/MedicalTreatment';
 
-{
-  /*
-  One instance of MedicalTreatment has been created.
-  Using props, the values stored in the medicalTreatment1 object
-  have been passed on to the MedicalTreatment.js component. 
-  */
-}
-
 export default function App() {
   const medicalTreatment1 = {
     treatId: '001',
     treatCourseId: '010',
-    type: 'alpha',
-    category: 'bravo',
-    name: 'charlie',
+    type: 'Oncology',
+    category: 'Nurse',
+    name: 'John',
     startDate: new Date(2021, 10, 19).toLocaleDateString(),
   };
   return (
--- src/components/MedicalTreatment.js
+++ src/components/MedicalTreatment.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-{
-  /*
-  React component MedicalTreatment containing the attributes passed through props.
-  This component's function is to print in a list all the attributes from props.
-  An instance of this component has been created through App.js.
-  */
-}
-
 function MedicalTreatment(props) {
   return (
     <div>
