4db77d952ff1979cba36622efdc57821c6520c49
Jose-Miguel-FT <miguelfinol_48@hotmail.com>
2021-12-08 18:54:10 +0000

    1 file modified

--- src/components/MedicalTreatment.js
+++ src/components/MedicalTreatment.js
@@ -3,9 +3,17 @@ import React from 'react';
 function MedicalTreatment(props) {
   return (
     <div>
-      {props.medicalTreatment.treatId}, {props.medicalTreatment.treatCourseId},{' '}
-      {props.medicalTreatment.type}, {props.medicalTreatment.category},{' '}
-      {props.medicalTreatment.name}, {props.medicalTreatment.startDate}
+      Treat ID: {props.medicalTreatment.treatId}
+      <br />
+      Treat Course ID: {props.medicalTreatment.treatCourseId}
+      <br />
+      Type: {props.medicalTreatment.type}
+      <br />
+      Category: {props.medicalTreatment.category}
+      <br />
+      Name: {props.medicalTreatment.name}
+      <br />
+      Start Date: {props.medicalTreatment.startDate}
     </div>
   );
 }
